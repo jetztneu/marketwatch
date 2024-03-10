d8ada15dae091e6c8a8730e3a3c6310b8878f222
jetztneu <webdev.agaur@gmail.com>
2024-03-10 02:59:37 +0100

    added gain toggle, update and refresh buttons, average slider, data follows chart toggle, removed ymd comparison for abs gain date start

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import yfinance as yf
 from datetime import datetime
 import time
 
+t0 = time.time()
 
 # SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
 st.set_page_config(layout="wide", page_title="Market Watch", page_icon=":money:")
@@ -19,14 +20,14 @@ st.set_page_config(layout="wide", page_title="Market Watch", page_icon=":money:"
 def load_data(download_new_data,exchanges,indexes,start_date,end_date):
     download_new_data = download_new_data
 
-    stock_exchanges = exchanges
+    exchanges = exchanges
     indexes = indexes
-    data_list = indexes
+    data_list = exchanges + indexes
 
     # start_date = start_date
     start_date = '1900-01-01'
-    end_date = end_date
-
+    end_date = datetime.today().strftime('%Y-%m-%d')
+    # st.sidebar.write(end_date)
     file_out = 'raw_data.xlsx'
     sheet_out = 'df_raw' 
     # Definiton of Dataframes
@@ -75,13 +76,18 @@ if st.sidebar.button('Update Database'):
 
 st.sidebar.button('Refresh')
 
-year = st.sidebar.slider('Year',1950,2024,(2020,2024))
+gain_toggle = st.sidebar.toggle('Gain %')
+
+year = st.sidebar.slider('Year',1991,2024,(2020,2024))
 # st.sidebar.year = st.slider('Year',1950,2024,(2020,2024))
-month = st.sidebar.slider('Month',1,12,(2,3))
-date = st.sidebar.slider('Date',1,31,(5,7))
+month = st.sidebar.slider('Month',1,12,2)
+date = st.sidebar.slider('Date',1,31,5)
 
 exchanges = ['BSE','DJI','Hong Kong']
-indexes = ['BSE','DJI','Nasdaq100','S&P500','FTSE100','Nifty50','DAX40','Euronext100']
+indexes = ['Nasdaq100','S&P500','FTSE100','Nifty50','DAX40','Euronext100']
+
+indian_market = ['BSE','Nifty50']
+world_market = ['DJI','Nasdaq100','S&P500','FTSE100','DAX40','Euronext100']
 
 data_list = []
 
@@ -110,11 +116,9 @@ if st.sidebar.checkbox('Euronext100'):
 
 # data_list = indexes
 
-
-
 s_year, e_year = year
-s_month, e_month = month
-s_date, e_date = date
+s_month, e_month = month, month
+s_date, e_date = date, date
 s_month, e_month, s_date, e_date = "{:02d}".format(s_month), "{:02d}".format(e_month), "{:02d}".format(s_date), "{:02d}".format(e_date)
 
 
@@ -125,61 +129,97 @@ end_date = str(e_year)+'-'+str(e_month)+'-'+str(e_date)
 abs_gain_date = start_date
 
 # Calculate the moving average with a window size of 3
-window_mov_avg = 7
+window_mov_avg = st.sidebar.slider('Average over days:',0,100,3)
 
 
 
 # Definiton of Dataframes
-
+df_raw_main = pd.DataFrame()
 df_raw = pd.DataFrame()
 df_mav = pd.DataFrame()
 df_normperyear = pd.DataFrame()
 df_normmax = pd.DataFrame()
 df_diff = pd.DataFrame()
 df_gain = pd.DataFrame()
+df_raw_re = pd.DataFrame()
 
 
-
-
-
-df_raw, file_out, sheet_out = load_data(download_new_data,exchanges,indexes,start_date,end_date)
-
-
-
+df_raw_main, file_out, sheet_out = load_data(download_new_data,exchanges,indexes,start_date,end_date)
+df_raw = df_raw_main.loc[start_date:end_date]
 
 ymd_abs_gain_start_val = abs_gain_date.split('-')[0]+abs_gain_date.split('-')[1]+abs_gain_date.split('-')[2]
 
 
 for data in data_list:
+    print(data)
     df_normperyear[data] = df_raw[data]/df_raw.groupby('Year')[data].transform('max')
     df_normmax[data] = df_raw[data]/max(df_raw[data].dropna())
     df_normmax[data] = df_normmax[data].rolling(window=window_mov_avg).mean()
     df_mav[data] = df_raw[data].rolling(window=window_mov_avg).mean()
     df_diff[data] = np.diff(df_raw[data])
-    df_gain[data] = ((df_raw[data] - df_raw.query('ymd==@ymd_abs_gain_start_val')[data][0])/df_raw.query('ymd==@ymd_abs_gain_start_val')[data][0]*100).rolling(window=window_mov_avg).mean()
+#    df_gain[data] = ((df_raw[data] - df_raw.query('ymd==@ymd_abs_gain_start_val')[data][0])/df_raw.query('ymd==@ymd_abs_gain_start_val')[data][0]*100).rolling(window=window_mov_avg).mean()
+    df_gain[data] = ((df_raw[data] - df_raw[data][0])/df_raw[data][0]*100).rolling(window=window_mov_avg).mean()
+    df_raw_re[data] = df_raw[data]
 
-
-sub_list = ['Nasdaq100','Nifty50']
-
-df_gain_sub = df_gain[sub_list]
 
 # Export data to excel
-
 if download_new_data == 1:
-    
     # Create a Pandas Excel writer using XlsxWriter as the engine.
     writer = pd.ExcelWriter(file_out, engine='xlsxwriter')
-    
     # Write each dataframe to a different worksheet.
-    df_raw.to_excel(writer, sheet_name=sheet_out)
+    df_raw_main.to_excel(writer, sheet_name=sheet_out)
     # df2.to_excel(writer, sheet_name='Sheet2')
-    
     # Close the Pandas Excel writer and output the Excel file.
     writer.save()
 
-st.title('Gain (%) from '+abs_gain_date+' to '+end_date)
-st.line_chart(df_gain)
 
 
-st.dataframe(df_raw)
-st.dataframe(df_gain)
+st.title(abs_gain_date+' to '+end_date)
+
+if gain_toggle == 1:
+    st.header('Gain (%)')
+    st.line_chart(df_gain)
+else:
+    st.header('Data')
+    st.line_chart(df_raw_re)
+
+
+
+col1, col2 = st.columns(2)
+
+with col1:
+   st.header("Indian Market")
+   df_gain_sub = pd.DataFrame()
+   df_raw_sub = df_raw[indian_market]
+   for data in data_list and indian_market:
+       df_gain_sub[data] = ((df_raw[data] - df_raw[data][0])/df_raw[data][0]*100).rolling(window=window_mov_avg).mean()
+
+   if gain_toggle == 1:   
+       st.line_chart(df_gain_sub)
+   else:
+       st.line_chart(df_raw_sub)
+   
+with col2:
+   st.header("World Market")
+   df_gain_sub = pd.DataFrame()
+   df_raw_sub = df_raw[world_market]
+   for data in data_list and world_market:
+       df_gain_sub[data] = ((df_raw[data] - df_raw[data][0])/df_raw[data][0]*100).rolling(window=window_mov_avg).mean()
+
+   if gain_toggle == 1:   
+       st.line_chart(df_gain_sub)
+   else:
+       st.line_chart(df_raw_sub)
+
+
+
+if st.sidebar.toggle('Data follow chart'):
+    st.dataframe(df_raw_re)
+else:
+    st.dataframe(df_raw_main)
+
+
+# st.dataframe(df_gain)
+fin = time.time()
+st.write('Took ',np.round(fin-t0,2),' s to load.')
+st.write(np.random.randint(0,10))
